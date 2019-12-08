17efc1a08942fed087be7a65bd910d07efa64e5d
tavisca-vnagaraju <vnagaraju@tavisca.in>
2019-12-08 14:21:18 +0530

    Add project files.

---
+++ FileStoreToDbStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileStoreToDbStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
